50e98da19c8aac0bb7f5172629f274e863dc0d8e
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-04 12:35:22 +0530

    add initial user route

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate incoming request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        // Create a new user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // Return response
+        return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
+    }
+}
