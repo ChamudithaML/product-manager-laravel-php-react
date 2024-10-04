061119fa06029f3a4eb16a6b0ae164650593e0d5
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-04 13:36:24 +0530

    complete login authentication with jwt

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
 class AuthController extends Controller
 {
     public function register(Request $request)
@@ -25,5 +28,26 @@ class AuthController extends Controller
 
         // Return response
         return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // Validate incoming request
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // Attempt to verify the credentials and create a token for the user
+        try {
+            if (!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // Return the token
+        return response()->json(compact('token'));
     }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
