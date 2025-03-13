06520b3302c025b4ef54fdbb2e8f7c81de7d556f
Chamuditha <lilan.2019660@iit.ac.lk>
2025-03-13 10:18:33 +0530

    new model for orders

---
+++ server/app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'products',
+        'total_amount',
+        'shipping_address',
+        'order_status',
+        'payment_status',
+    ];
+}
